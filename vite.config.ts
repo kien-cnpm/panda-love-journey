a00919bcbdf8fde806b29c45821997dce844b96f
Trung Kien <kien36503@gmail.com>
2025-08-29 13:58:17 +0700

    lucky

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/panda-love-journey/",
   server: {
     host: "::",
     port: 3000,
